ab916bfe468fc52a0f8ef87ab2ad8fd9997db8fa
Memoriam.Alzheimer <memoriam.alzheimer@gmail.com>
2025-10-26 12:21:33 -0300

    Atualiza app.js para usar porta do Render

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = require('./config/express')();
 require('./app/routes/usuarios')(app);
 
-const PORT = process.env.PORT || 3000;
+ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function() {
     console.log(`Servidor rodando na porta ${PORT}`);
