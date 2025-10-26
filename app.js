f5c867743d0013bf4da670afe2e1e2cffa6c51ff
Memoriam.Alzheimer <memoriam.alzheimer@gmail.com>
2025-10-26 11:37:53 -0300

    Corrige porta para Render e adiciona config/express.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var app = require('./config/express')();
 require('./app/routes/usuarios')(app);
 
-const PORT = process.env.PORT || 3000;  
-app.listen(PORT, function(){
+// Usa a porta do Render ou 3000 como fallback
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
